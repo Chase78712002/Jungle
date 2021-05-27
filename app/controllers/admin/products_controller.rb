7cb5c9b9578d1d4937bd279069d35dea5a151954
Alvin Lin <alvinch.lin@gmail.com>
2021-05-27 10:15:13 -0700

    Turns out all I needed to do was restart the server. the ENV worked.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name:"Jungle", password: "book"
+  http_basic_authenticate_with name:ENV['AUTHENTICATION_USERNAME'], password: ENV['AUTHENTICATION_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
