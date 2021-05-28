cf8a07307e0ed04505885489e450543b65bf3086
Alvin Lin <alvinch.lin@gmail.com>
2021-05-27 19:16:02 -0700

    Update session controller with the login logout logic.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,19 @@ def new
 end
 
 def create
+  user = User.find_by_email(params[:email])
+
+  if user && .authenticate(params[:password])
+    session[:user_id] = user.id 
+    redirect_to '/'
+  else
+    redirect_to '/login'
 end
 
 
 def destroy
+  session[:user_id] = nil
+  redirect_to '/login'
 end
 
 
