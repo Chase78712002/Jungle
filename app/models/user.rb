701382f6d22ecec634936ebf554ae8f48785e332
Alvin Lin <alvinch.lin@gmail.com>
2021-06-01 14:28:05 -0700

    Define authentication class method specs. Implement new class method for authenticating user credentials.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,23 @@
 class User < ActiveRecord::Base
 
+  
+
+
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: { case_sensitive: false}
   validates :password, presence: true, length: { minimum: 3}
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    stripped_downcase_email = email.strip.downcase
+    @user = User.find_by_email(stripped_downcase_email)
+    if @user && @user.authenticate(password)
+      return @user
+    else
+      return nil
+    end
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -85,6 +85,74 @@ RSpec.describe User, type: :model do
         expect(user1.errors.full_messages[0]).to eq("Password is too short (minimum is 3 characters)")
       end
     end
+  end
 
+  describe ".authenticate_with_credentials" do
+    context "Taking email and password from user input as arguements" do
+      it "should return an instance of the user" do
+        user = User.new(
+          name: "Igudala",
+          email: "andreig@mail.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        user.save
+        user1 = User.authenticate_with_credentials("andreig@mail.com", "123")
+
+        expect(user1).to eq(user)
+      end
+
+      it "should return nil if email not authenticated" do
+        user = User.new(
+          name: "Igudala",
+          email: "andreig@mail.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        user.save
+        user1 = User.authenticate_with_credentials("andreig123@mail.com", "123")
+
+        expect(user1).to be_nil
+      end
+
+      it "should return nil if password not authenticated" do
+        user = User.new(
+          name: "Igudala",
+          email: "andreig@mail.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        user.save
+        user1 = User.authenticate_with_credentials("andreig@mail.com", "456")
+
+        expect(user1).to be_nil
+      end
+
+      it "should be able to authenticate with leading and trailing spaces in email entered" do
+        user = User.new(
+          name: "Igudala",
+          email: "andreig@mail.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        user.save
+        user1 = User.authenticate_with_credentials(" andreig@mail.com ", "123")
+
+        expect(user1).to eq(user)
+      end
+
+      it "should be able to authenticate with different case in email entered" do
+        user = User.new(
+          name: "Igudala",
+          email: "andreig@mail.com",
+          password: "123",
+          password_confirmation: "123"
+        )
+        user.save
+        user1 = User.authenticate_with_credentials("ANDREIG@mail.Com", "123")
+
+        expect(user1).to eq(user)
+      end
+    end
   end
 end
